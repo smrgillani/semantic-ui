58b0e5219cb0fdf0802b62254262d3d40d472f45
Dion Yang <jellopops@gmail.com>
2019-05-10 11:00:17 +0700

    initial skeleton

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./src/build'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+
+/**
+ * Default task
+ */
+gulp.task('default', ['sass:watch']);
